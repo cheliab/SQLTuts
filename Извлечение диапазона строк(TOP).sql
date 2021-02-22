ad82362b7dd2d0c38dc0334b291f2da3f24d9b94
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-02-22 10:24:32 +1000

    Добавлен пример использования TOP

---
+++ Извлечение диапазона строк(TOP).sql
@@ -0,0 +1,39 @@
+-- Извелечение диапазона строк
+
+create database tut_Top
+go
+
+use tut_Top
+go
+
+create table Products
+(
+	Id int identity primary key,
+	ProductName nvarchar(30) not null,
+	Manufacturer nvarchar(20) not null,
+	ProductCount int default 0,
+	Price money not null
+)
+
+insert into Products
+values
+('iPhone 6', 'Apple', 3, 36000),
+('iPhone 6S', 'Apple', 2, 41000),
+('iPhone 7', 'Apple', 5, 52000),
+('Galaxy S8', 'Samsung', 2, 46000),
+('Galaxy S8 Plus', 'Samsung', 1, 56000),
+('Mi6', 'Xiaomi', 5, 28000),
+('OnePlus 5', 'OnePlus', 6, 38000)
+
+--------------------------------------------
+-- Оператор TOP
+
+select top 4 ProductName
+from Products
+
+--------------------------------------------
+-- Оператор PERCENT
+
+select top 50 percent ProductName
+from Products
+
