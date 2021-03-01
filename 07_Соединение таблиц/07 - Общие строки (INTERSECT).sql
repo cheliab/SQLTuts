796c896e19f5129ffe1ebe16163c3b45bd9c0901
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-03-01 23:01:38 +1000

    Пример нахождения общих строк в таблицах (INTERSECT)

---
+++ 07_Соединение таблиц/07 - Общие строки (INTERSECT).sql
@@ -0,0 +1,51 @@
+create database tut_Intersect
+go
+
+use tut_Intersect
+go
+
+------------------------------------------
+
+CREATE TABLE Customers
+(
+    Id INT IDENTITY PRIMARY KEY,
+    FirstName NVARCHAR(20) NOT NULL,
+    LastName NVARCHAR(20) NOT NULL,
+    AccountSum MONEY
+);
+CREATE TABLE Employees
+(
+    Id INT IDENTITY PRIMARY KEY,
+    FirstName NVARCHAR(20) NOT NULL,
+    LastName NVARCHAR(20) NOT NULL,
+);
+
+------------------------------------------
+ 
+INSERT INTO Customers VALUES
+('Tom', 'Smith', 2000),
+('Sam', 'Brown', 3000),
+('Mark', 'Adams', 2500),
+('Paul', 'Ins', 4200),
+('John', 'Smith', 2800),
+('Tim', 'Cook', 2800)
+ 
+INSERT INTO Employees VALUES
+('Homer', 'Simpson'),
+('Tom', 'Smith'),
+('Mark', 'Adams'),
+('Nick', 'Svensson')
+
+------------------------------------------
+
+select 
+FirstName,
+LastName
+from Employees
+
+intersect
+
+select
+FirstName,
+LastName
+from Customers
