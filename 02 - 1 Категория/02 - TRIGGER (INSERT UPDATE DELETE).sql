d0c8f245f060b1889fc93c97a66ccc13afce892d
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-03-17 01:59:18 +1000

    Добавлены пару примеров создания триггеров

---
+++ 02 - 1 Категория/02 - TRIGGER (INSERT UPDATE DELETE).sql
@@ -0,0 +1,82 @@
+﻿create database tut_Trigger_INSERT_UPDATE_DELETE
+go
+
+use tut_Trigger_INSERT_UPDATE_DELETE
+go
+
+---------------------------------
+
+create table Products (
+	Id int identity primary key,
+	ProductName nvarchar(30) not null,
+	Manufacturer nvarchar(30) not null,
+	ProductCount int default 0,
+	Price decimal(19,4) not null
+)
+go
+
+create table History (
+	Id int identity primary key,
+	ProductId int not null,
+	Operation nvarchar(200) not null,
+	CreateAt datetime not null default getdate()
+)
+go
+
+---------------------------------
+-- INSERT - Триггер для добавления записи в историю при добалении нового товара
+
+create trigger Products_INSERT
+on Products
+after insert
+as
+insert into History (ProductId, Operation)
+select Id, 'Добавлен товар: ' + ProductName + '. Фирма: ' + Manufacturer
+from inserted
+
+-- Проверка работы триггера
+
+insert into Products
+(ProductName, Manufacturer, ProductCount, Price)
+values
+('iPhone X', 'Apple', 2, 79900)
+go
+
+select * from History
+go
+
+-----------------------------------
+-- DELETE - Триггер для добавлении записи в историю при удалении из таблицы товаров
+
+create trigger Products_DELETE
+on Products
+after delete
+as
+insert into History (ProductId, Operation)
+select Id, 'Удален товар: ' + ProductName + ' Фирма: ' + Manufacturer
+from deleted
+go
+
+-- Проверка работы триггера
+delete from Products
+where Id = 1
+go
+
+select * from History
+go
+
+----------------------------------------------------
+-- UPDATE - Триггер на обновление данных
+
+create trigger Product_UPDATE
+on Products
+after update
+as
+insert into History (ProductId, Operation)
+select Id, 'Обновлен товар: ' + ProductName + ' Фирма: ' + Manufacturer
+from inserted
+go
+
+--insert
+
+--update 
---
+++ 02 - 1 Категория/02 - TRIGGER.sql
@@ -0,0 +1,59 @@
+﻿-- Создаем БД
+
+create database tut_Trigger
+go
+
+use tut_Trigger
+go
+
+--------------------------------------------
+-- Создаем таблицу
+
+create table Products (
+	Id int identity primary key,
+	ProductName nvarchar(30) not null,
+	Manufacturer nvarchar(30) not null,
+	ProductCount  int default 0,
+	Price decimal(19,4) not null
+)
+go
+
+---------------------------------------------
+-- Создаем триггер для таблицы Products
+
+-- Формальное определение
+-- CREATE TRIGGER имя_триггера
+-- ON { имя_таблицы | имя_представления }
+-- {AFTER | INSTEAD} [INSERT | UPDATE | DELETE]
+-- AS выражения_sql
+
+create trigger Products_INSERT_UPDATE
+on Products
+after insert, update
+as
+update Products
+set Price = Price + Price * 0.38 -- С помощью триггера определяем новую цену, на основе чистой цены
+where Id = (select Id from inserted)
+
+insert into Products
+(ProductName, ProductCount, Manufacturer, Price)
+values
+('iPhone X', 2, 'Apple', 68000)
+
+select * from Products
+
+-------------------------------------
+-- Удаление триггера
+
+drop trigger Products_INSERT_UPDATE
+go
+
+-------------------------------------
+-- Отключение / Включение триггера
+
+disable trigger Products_INSERT_UPDATE on Products -- откл.
+go
+
+enable trigger Products_INSERT_UPDATE on Products -- вкл.
+go
+
