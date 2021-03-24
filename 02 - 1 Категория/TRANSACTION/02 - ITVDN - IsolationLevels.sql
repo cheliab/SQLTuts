38262d562b2d013cdb7f595bd14e04ef2a80efbf
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-03-25 07:58:15 +1000

    Наброски примера с уровнями изоляции

---
+++ 02 - 1 Категория/TRANSACTION/02 - ITVDN - IsolationLevels.sql
@@ -0,0 +1,39 @@
+﻿create database tut_ITVDN_IsolationLevels
+go
+
+use tut_ITVDN_IsolationLevels
+go
+
+----------------------------------------------
+-- Подготовка таблицы для примеров с уровнями изоляции
+
+if exists(select 1 from sys.tables where object_id = OBJECT_ID('TestTable'))
+	drop table TestTable
+	
+
+create table TestTable (
+	[Id] int identity,
+	[Value] int
+)
+go
+
+insert into TestTable values
+(1)
+go
+
+---------------------------------------------
+-- 1. LOST UPDATE / ПОТЕРЯННОЕ ОБНОВЛЕНИЕ
+
+begin tran
+
+update TestTable
+set [Value] = [Value] + 3
+where Id = 1;
+
+waitfor delay '00:00:10';
+
+select [Value]
+from TestTable
+where Id = 1;
+
+commit tran;
