b5e92adce0b6608b854359778dd4b42ea3017e1f
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-03-31 07:56:15 +1000

    Наброски для агрегатных функций

---
+++ 02 - 1 Категория/02 - SYSTEM CATALOG - AGGREGATE FUNCTIONS/02 - Aggreagate functions.sql
@@ -0,0 +1,20 @@
+﻿create database tut_AggregateFunctions
+go
+
+use tut_AggregateFunctions
+go
+
+----------------------------------------
+
+create table Orders (
+	Id int primary key identity,
+	[Date] datetime not null,
+	ProductId int not null,
+	Qty int not null,
+	Price decimal(6, 4) not null,
+	City nvarchar(50) 
+)
+
+insert into Orders
+values
+('', 1, 10, 500, 'Владивосток')
