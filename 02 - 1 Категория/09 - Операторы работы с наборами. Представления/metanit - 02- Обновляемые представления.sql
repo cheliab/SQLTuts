3e7f5204785c438114391c194cc2e4c2d8a9436c
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-04-05 00:22:34 +1000

    Добавил пример с обновляемым представлением

--- 02 - 1 Категория/09 - Операторы работы с наборами. Представления/metanit - 02- Обновляемые представления.sql
+++ 02 - 1 Категория/09 - Операторы работы с наборами. Представления/metanit - 02- Обновляемые представления.sql
@@ -6,3 +6,42 @@ go
 
 -------------------------------------------------------------
 
+create table Products (
+	Id int identity primary key,
+	ProductName nvarchar(30) not null,
+	Manufacturer nvarchar(20) not null,
+	ProductCount int default 0,
+	Price decimal(16, 2) not null
+)
+go
+
+---------------------------------------------------------
+-- создаем представление
+
+create view ProductView as
+select 
+	ProductName Product,
+	Manufacturer,
+	Price
+from Products
+go
+
+------------------------------------------------------------
+-- вставляем данные
+
+insert into ProductView (Product, Manufacturer, Price)
+values ('Nokia 8', 'HDC Global', 1000)
+go
+
+------------------------------------------------------------
+-- проверяем
+
+select * from ProductView
+
+------------------------------------------------------------
+-- обновление
+
+update ProductView
+set Price = 1200
+where Product = 'Nokia 8'
+
