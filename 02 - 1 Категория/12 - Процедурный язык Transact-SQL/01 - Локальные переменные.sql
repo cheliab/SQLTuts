f6bc9dbcd66c92ced4f45307646815ca33c1d810
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-04-06 08:11:29 +1000

    Добавил примеров с переменным

--- 02 - 1 Категория/12 - Процедурный язык Transact-SQL/01 - Локальные переменные.sql
+++ 02 - 1 Категория/12 - Процедурный язык Transact-SQL/01 - Локальные переменные.sql
@@ -1,4 +1,30 @@
-﻿declare @name varchar(10);
+﻿create database metanit_LocalVariables
+go
+
+use metanit_LocalVariables
+go
+
+---------------------------------------------------------
+
+create table Person (
+	Id int identity,
+	FirstName nvarchar(30)
+)
+
+insert into Person 
+values
+('Макс'),
+('Павел')
+
+insert into Person 
+values
+('Вика'),
+('Дима'),
+('Леша')
+
+---------------------------------------------------------
+
+declare @name varchar(10);
 print isnull(@name, 'NULL'); -- по умолчанию значение переменной NULL
 
 declare @age smallint = 2;
@@ -12,6 +38,27 @@ print @name;
 -- GO
 
 declare @fullname varchar(10) = 'Pavel Berezkin';
-go -- переменная существует только в рамках одного пакета (при выхове после GO будет ошибка)
+go -- переменная существует только в рамках одного пакета (при вызове переменной после GO уже будет ошибка)
 
 print isnull(@fullname, 'NULL'); -- возникнет ошибка, переменной @fullname уже нет
+
+-- SELECT
+
+declare @name nvarchar(30);
+
+select @name = 'Макс' -- Также можно использовать оператор SELECT для присвоения значения переменной 
+
+print @name
+go
+
+-- SELECT ... FROM
+
+declare @name nvarchar(30)
+
+select @name = FirstName from Person where Id = 2 -- Можно получить зачение из таблицы
+
+print @name
+
+select @name = FirstName from Person -- Если не указываем условие, то будет присвоено значение из последней строки
+
+print @name
