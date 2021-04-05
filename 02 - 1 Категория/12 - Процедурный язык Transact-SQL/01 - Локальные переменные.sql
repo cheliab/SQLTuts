ed60c1a63ca4c6da37559e90a5e6728d67d515c5
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-04-05 10:08:54 +1000

    Начал делать примеры по процедурному языку

---
+++ 02 - 1 Категория/12 - Процедурный язык Transact-SQL/01 - Локальные переменные.sql
@@ -0,0 +1,17 @@
+﻿declare @name varchar(10);
+print isnull(@name, 'NULL'); -- по умолчанию значение переменной NULL
+
+declare @age smallint = 2;
+print @age;
+
+-- SET
+
+set @name = 'Elijah';
+print @name;
+
+-- GO
+
+declare @fullname varchar(10) = 'Pavel Berezkin';
+go -- переменная существует только в рамках одного пакета (при выхове после GO будет ошибка)
+
+print isnull(@fullname, 'NULL'); -- возникнет ошибка, переменной @fullname уже нет
