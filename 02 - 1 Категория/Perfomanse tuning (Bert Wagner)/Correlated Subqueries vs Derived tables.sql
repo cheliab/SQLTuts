e8853cb0190ab08abda8991d89ff516d2e8f2859
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-04-12 09:30:50 +1000

    Добавил пример с оптимизаций запроса

---
+++ 02 - 1 Категория/Perfomanse tuning (Bert Wagner)/Correlated Subqueries vs Derived tables.sql
@@ -0,0 +1,81 @@
+﻿-- Correlated Subqueries vs Derived Tables
+-- https://www.youtube.com/watch?v=Kx4U5BhRGqM
+
+-----------------------------------------------------------
+
+create database BertWagner_CorrelatedSubqueriesVSDerivedTables
+go
+
+use BertWagner_CorrelatedSubqueriesVSDerivedTables
+go
+
+create table Badges (
+	Id int identity primary key,
+	UserId int,
+	[Date] date
+)
+
+create nonclustered index IX_Badges_UserID_Date
+on Badges (UserId ASC)
+
+drop table Badges
+
+-----------------------------------------------------------
+
+declare @counter1 int = 1;
+declare @counter2 int = 1;
+declare @randDate date;
+declare @userID int;
+
+while (@counter1 <= 10)
+begin
+	set @counter2 = 1;
+
+	set @userID = @counter1
+
+	while (@counter2 <= 20)
+	begin
+		
+		set @randdate = DATEADD(DAY, ABS(CHECKSUM(NEWID()) % 364 ), '2011-01-01');
+
+		insert into Badges (UserId, [Date]) values (@userID, @randDate)
+
+		set @counter2 = @counter2 + 1;
+
+	end
+
+	set @counter1 = @counter1 + 1;
+
+end
+
+select * from Badges
+
+-----------------------------------------------------------
+-- Оригинальный запрос
+
+select distinct
+	UserId,
+	FirstBadgeDate = 
+		(select min([Date])
+		from Badges i 
+		where o.UserId = i.UserId)
+from
+	Badges o
+
+-- такое написание генерирует план запрос с большим количетсвом лишних чтений данных
+-- данные агрегируются только в самом конце, до этого момента манипуляции происходят со всеми строками
+
+-----------------------------------------------------------
+-- Новый вариант запроса
+
+select distinct
+	o.UserId,
+	FirstBadgeDate
+from
+	Badges o
+		inner join (
+			select UserId, MIN([Date]) as FirstBadgeDate
+			from Badges
+			group by UserId
+		) i
+		on o.UserId = i.UserId
