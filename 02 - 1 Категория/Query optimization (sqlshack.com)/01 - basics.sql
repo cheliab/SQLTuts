1f6b54f0b8d2f44ef89220e28feda81807d9b38e
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-04-15 10:55:37 +1000

    Добавил несколько примеров оптимизаций с сайта sqlshack
    basic + tips and tricks

---
+++ 02 - 1 Категория/Query optimization (sqlshack.com)/01 - basics.sql
@@ -0,0 +1,91 @@
+﻿-- Примеры оптимизаций
+-- https://www.sqlshack.com/query-optimization-techniques-in-sql-server-the-basics/
+
+create database sqlshack_Basic;
+go
+
+use sqlshack_Basic;
+go
+
+----------------------------------------------
+
+create table Person (
+	Id int identity primary key,
+	FirstName nvarchar(50),
+	LastName nvarchar(50),
+	MiddleName nvarchar(50)
+	index IX_Person_LastName_FirstName_MiddleName nonclustered (LastName, FirstName, MiddleName)
+)
+
+insert into Person
+values
+('Иван', 'Иванов', 'Иванович'),
+('Иван', 'Смирнов', 'Иванович'),
+('Иван', 'Петров', 'Иванович'),
+('Иван', 'Шевцов', 'Иванович'),
+('Иван', 'Орлов', 'Иванович'),
+('Иван', 'Скворцов', 'Иванович'),
+('Иван', 'Удальцов', 'Иванович'),
+('Иван', 'Казанцев', 'Иванович')
+
+-----------------------------------------------
+-- 1. Использование функций в условиях
+
+select
+	Id,
+	FirstName,
+	LastName,
+	MiddleName
+from Person
+where
+	LEFT(LastName, 3) = 'Орл' -- При использовании функции для колонки будет выполнено полное сканирование таблицы
+
+-- Чтобы выполнялся поиск по индексу нужно переписать запрос без использования функции для колонки
+
+select
+	Id,
+	FirstName,
+	LastName,
+	MiddleName
+from Person
+where
+	LastName like 'Орл%' -- при таком написании будет использован поиск по индексу
+
+----------------------------------------------
+-- 2. Неявное преобразование типов
+
+create table Employee (
+	Id varchar(5) primary key,
+	LoginId int,
+	JobTitle nvarchar(20)
+)
+
+insert into Employee
+values
+('123', 1, 'Программист'),
+('234', 2, 'Аналитик'),
+('345', 3, 'Руководитель'),
+('456', 4, 'Продукт менеджер'),
+('567', 5, 'Генеральный директор'),
+('678', 6, 'Консультант')
+
+----------------------------------------------
+
+select
+	Id,
+	LoginId,
+	JobTitle
+from Employee
+where
+	Id = 567 -- при таком написании условия будет происходить неявное преобразование типа и будет выполнено сканирование
+
+-- Нужно переписать условие, чтобы не было преобразования типов
+
+select
+	Id,
+	LoginId,
+	JobTitle
+from Employee
+where
+	Id = '567' -- При таком написании не будет преобразований и будет выполнен поиск по индексу
+
---
+++ 02 - 1 Категория/Query optimization (sqlshack.com)/02 - tips and tricks.sql
@@ -0,0 +1,62 @@
+﻿-- Примеры оптимизаций
+https://www.sqlshack.com/query-optimization-techniques-in-sql-server-tips-and-tricks/
+
+create database sqlshack_TipsAndTricks
+go
+
+use sqlshack_TipsAndTricks
+go
+
+---------------------------------------------------
+-- 1. Использование OR (ИЛИ) в соединениях
+
+create table Product (
+	ProductID int identity primary key,
+	RowGUID int,
+	[Name] nvarchar(50)
+)
+
+create table SalesOrderDetail (
+	RowGUID int,
+	ProductID int
+	index PK_SalesOrderDetail_SalesOrderID clustered (RowGUID, ProductID)
+)
+
+insert into Product -- 6
+values
+(1, 'iPhone 1'),
+(2, 'iPhone 2'),
+(3, 'iPhone 3'),
+(4, 'iPhone 4'),
+(5, 'iPhone 5'),
+(6, 'iPhone 6')
+
+
+insert into SalesOrderDetail -- 12
+values
+(1, 1),
+(1, 2),
+(2, 2),
+(2, 5),
+(3, 3),
+(3, 1),
+(4, 4),
+(4, 3),
+(5, 5),
+(5, 1),
+(6, 1),
+(6, 6)
+
+----------------------------------------------------------------------
+-- 1. Использование OR (ИЛИ) в соединениях
+
+select distinct
+	P.ProductID,
+	P.[Name]
+from Product P
+	inner join SalesOrderDetail D -- при таком написани будет много лишних чтений из таблицы SalesOrderDetail
+	on P.ProductID = D.ProductID
+	or P.RowGUID = D.RowGUID
+
+-- Можно переписать с использованием UNION
+
